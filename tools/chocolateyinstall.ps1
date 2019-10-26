d2c3799d322d8caafb885a4589f04b362aa7afa9
Justen Walker <justen.walker+github@gmail.com>
2019-10-26 14:25:33 -0400

    v5.2.0 Installer

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = 'https://github.com/trustcrypto/OnlyKey-App/releases/download/v5.2.0/OnlyKey_5.2.0.exe'
+  softwareName  = 'OnlyKey*'
+  checksum      = '71ae01f86995c1aadae947d447223f65540c0c5ebcd1319dd5e5f1e1907013c0'
+  checksumType  = 'sha256'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
